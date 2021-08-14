beeaa5bdef479d771c67b16325ee44000a6ca1f0
Rodrigo Grzegorczyk <rodrigo.gregorio1@hotmail.com>
2021-08-13 21:48:03 -0300

    Desafio concluido

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -9,7 +9,21 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const userExist = this.usersRepository.findById(user_id);
+
+    if(!userExist) {
+      throw new Error("User not exists");
+    }
+
+    const userIsAdmin = userExist.admin;
+
+    if(userIsAdmin === false) {
+      throw new Error("User is not a admin");
+    } 
+
+    const user = this.usersRepository.list();
+
+    return user;
   }
 }
 
